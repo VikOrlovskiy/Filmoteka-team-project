a3eb719a6736b71cbf7ac10a428788bbe33fdb63
vik_orel <vikorel34@gmail.com>
2022-01-07 19:32:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+// ==========styles=================
 import './sass/main.scss';
+// ==========Document Refs===========
+import "./js/Refs";
+// ==========Document HEADER=========
+// import "./js/header";
+// import "./js/navigation";
+// ==========Document Auth=========
+// import "./js/fireBaseApi";
+// ==========Document MAIN========
+// import "./js/gallery";
+// ==========Document FOOTER=========
+// import "./js/footer";
