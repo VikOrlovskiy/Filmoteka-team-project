a8f1987c75a8f92f91e35e826323bdfad4892107
vik_orel <vikorel34@gmail.com>
2022-01-07 19:32:56 +0200

    Create Refs.js

---
+++ src/js/Refs.js
@@ -0,0 +1,27 @@
+export default  {
+    // body: document.querySelector('body'),
+    // =====================header=================================
+    // headerContainer:document.querySelector('.container-header'),
+    // =====================sitename===============================
+    // backHomePage:document.querySelector('.name'),
+    // =====================navigation panel=======================
+    // navigationPanel: document.querySelector('.navigation'),
+    // userButtons:document.querySelector('.header-button-block'),
+    // ===============search=======================================
+    // searchBlock: document.querySelector('.search'),
+    // searchInput: document.querySelector('.search_input'),
+    // errorTextField:document.querySelector('.search_text'),
+    // =====================main====================================
+    // galleryRef:document.getElementById('gallery'),
+    // =======================modal=================================
+    // backDrop: document.querySelector('.back-drop'),
+    // modalWindow: document.querySelector('.modal'),
+    // modalButtonClose:document.querySelector('.modal_close'),
+    // modalContentContainer: document.querySelector('.container-modal'),
+    // =======================pagination============================
+    // paginationContainer:document.getElementById('pagination'),
+    // =======================footer================================
+    // footerButtonTeamModal: document.querySelector('.footer_button'),
+
+  };
+  
