4bd0e2ec978d098cc8752a7d51a1b17abec796bb
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-01-14 15:12:32 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 // ==========styles=================
 import './sass/main.scss';
 // ==========Document Refs===========
-import "./js/Refs";
+import './js/Refs';
 // ==========Document HEADER=========
-import "./js/FetchMovieServise";
-import "./js/renderUserLibrary";
+import './js/FetchMovieServise';
+import './js/renderUserLibrary';
 // import "./js/navigation";
 // ==========Document Auth=========
 // import "./js/fireBaseApi";
 // ==========Document MAIN========
 // import "./js/gallery";
+import './js/pagination';
 // ==========Document FOOTER=========
 // import "./js/footer";
