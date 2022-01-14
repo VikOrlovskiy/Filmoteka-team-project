8244f1882a576795aeb4d7d7e33743ebf743c3ab
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-01-14 15:49:45 +0200

    pagination-fixed

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,23 +2,26 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import ref from './Refs';
 
-const options = {
-  totalItems: 200,
-  itemsPerPage: 10,
-  visiblePages: 10,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-};
+function tuiPagination() {
+  const options = {
+    totalItems: 200,
+    itemsPerPage: 10,
+    visiblePages: 10,
+    page: 1,
+    centerAlign: false,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+  };
 
-const pagination = new Pagination(ref.paginationRef, options);
+  const pagination = new Pagination(ref.paginationRef, options);
 
-pagination.on('beforeMove', event => {
-  const currentPage = event.page;
+  pagination.on('beforeMove', event => {
+    const currentPage = event.page;
 
-  if (currentPage === 10) {
-    return false;
-  }
-  console.log(currentPage);
-});
+    if (currentPage === 10) {
+      return false;
+    }
+    console.log(currentPage);
+  });
+}
+tuiPagination();
