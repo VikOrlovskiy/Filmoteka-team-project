989c69d50b651d9110d114519bd2ee4b1623f5aa
SerhBondarenko <serhii.bondarenk@gmail.com>
2022-01-17 22:55:50 +0200

    fix-enptySearchValue
    При пустом з  инпуте  кнопка поиска становиться  disabled

--- src/js/serchFilmByValue.js
+++ src/js/serchFilmByValue.js
@@ -4,27 +4,28 @@ const dataFetch = new DataFetch();
 import ref from './Refs';
 import { showLoader, hideLoader } from './loader'
 
+ref.buttonFilmSerch.disabled = true;
 
 ref.formFilmSerch.addEventListener('submit', onSearch);
-
+ref.inputFilmSerch.addEventListener('input', onSearchInput);
 function onSearch (e)  {
   e.preventDefault();
   dataFetch.query = e.currentTarget.elements.query.value.trim();
-  clearFetchResault() 
-  if (DataFetch.query === "") {
-    clearFetchResault()
-  }
-  else {    
     dataFetch
       .fetchFilms()
       .then(renderMovieCardFilms)
       .catch(error => {
         console.log(error.message);
       });
+};
+
+function onSearchInput(e) {
+  e.preventDefault();
+  if (!e.currentTarget.value) {
+    ref.buttonFilmSerch.disabled = true;
+    dataFetch.fetchTopFilms().then(renderMovieCardFilms)
   }
-};
-
-function clearFetchResault() {
-  ref.galleryRef.innerHTML = "";
-};
-
+  else {
+    ref.buttonFilmSerch.disabled = false;
+  }
+}
