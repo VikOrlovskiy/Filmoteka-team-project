215c1db35cf57c5e4c77f75dbe60666f9a17bbf3
SerhBondarenko <serhii.bondarenk@gmail.com>
2022-01-17 10:07:57 +0200

    bug-fix1
    1.Создал отдельный файл для рендера фильмов.
    2.В hbs файле добавил img если картинка не загружается.
    3. Для img поставил фиксированную высоту.

---
+++ src/js/normaliseRenderFilm.js
@@ -0,0 +1,41 @@
+import refs from './Refs.js';
+import movieCardTpl from '../templates/fetchMovieTemplate.hbs';
+import DataFetch from './filmServiceApi';
+
+function renderMovieCardFilms(films) {
+  return films.results.map(
+    ({ original_title, release_date, poster_path, genre_ids, vote_average, id }) => {
+      let changedYear = changeYear(release_date);
+      let filmGenry = changeGenrys(genre_ids);
+      refs.galleryRef.insertAdjacentHTML(
+        'afterbegin',
+        movieCardTpl({
+          original_title,
+          changedYear,
+          poster_path,
+          filmGenry,
+          vote_average,
+          id,
+        }),
+      );
+    },
+  );
+}
+function changeYear(release_date) {
+  return release_date.slice(0, 4);
+}
+
+function changeGenrys(genre_ids) {
+  let genreList = genre_ids
+    .map(id => DataFetch.genres.filter(genre => genre.id === id).map(genre => genre.name))
+    .flat();
+  if (genreList.length === 0) {
+    return (genreList = [`Unknown`]);
+  }
+  if (genreList.length >= 1 && genreList.length <= 2) {
+    return genreList.slice(0, 2);
+  } else {
+    return (genreList = `${genreList[0]}, ${genreList[1]}, Other`);
+  }
+}
+export { renderMovieCardFilms };
