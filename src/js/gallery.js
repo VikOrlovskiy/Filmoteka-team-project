16c2c1d36cc3cfa67e2e0efddd1a89f03b523ce8
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-01-18 19:24:46 +0200

    pagination_gallery
    Видалиш файл pagination.js він вже непотрібен

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,13 +1,14 @@
-import DataFetch from "./filmServiceApi.js";
+import DataFetch from './filmServiceApi.js';
 import { renderMovieCardFilms } from './normaliseRenderFilm';
 const dataFetch = new DataFetch();
+import { tuiPagination } from './paginationGallery';
 
 window.addEventListener('load', loadTrendingFilms);
 // ========================ЗАГРУЗКА ПОПУЛЯРНЫХ ФИЛЬМОВ====================
 async function loadTrendingFilms() {
   await DataFetch.fetchGenres();
   await dataFetch.fetchTopFilms().then(films => {
-    renderMovieCardFilms(films)
+    renderMovieCardFilms(films);
   });
-};
-
+  tuiPagination();
+}
