b8863b20fb19fcc708101303e94ea0d567ddd330
vik_orel <vikorel34@gmail.com>
2022-01-19 15:20:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './js/Refs';
 // ==========Document HEADER=========
 import './js/serchFilmByValue';
 import './js/renderUserLibrary';
+import './js/userFunctions';
 // ==========Document Auth=========
 // import "./js/fireBaseApi";
 // ==========Document MAIN========
