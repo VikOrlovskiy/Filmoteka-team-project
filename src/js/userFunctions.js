bd37ea5838d0a138c5b30181be334d78ce929c2f
vik_orel <vikorel34@gmail.com>
2022-01-20 10:22:54 +0200

    Update userFunctions.js

--- src/js/userFunctions.js
+++ src/js/userFunctions.js
@@ -1,4 +1,4 @@
-import { authWithEmailAndPassword, RegistrationWithEmailAndPassword,logOutAuthUser,authState} from "./fireBaseApi";
+import { authWithEmailAndPassword, RegistrationWithEmailAndPassword,logOutAuthUser,authState,readUserData,writeUserData} from "./fireBaseApi";
 import Refs from "./Refs";
 import {actionPopUp} from './actionPopUp'
 import formLogIn from '../templates/formLogIn.hbs';
@@ -9,7 +9,6 @@ Refs.logOutButton.addEventListener('click' , logOutAuthUser)
 Refs.logInButton.addEventListener('click' , onClickregistrationOrlogInUser)
 // ==================User auth State========================
 authState()
-
 function onClickregistrationOrlogInUser(){
     renderForm(formLogIn)
     Refs.popUp.classList.add("modal_form")
@@ -52,30 +51,35 @@ function onRegistrationLinkClick(e){
 }
 export{onSubmitRegistrationForm,onSubmitEntryForm,renderForm,onRegistrationLinkClick}
 let dataUser = JSON.parse(localStorage.getItem('userData'))
-console.log(dataUser)
-// if(dataUser !== null){
-//     writeToFBHundler(dataUser.accessToken,'films',dataUser.uid,[34545667788,345678,])
-// }
-function writeToFBHundler(accessToken,nameCollection,uid,Collection) {
-    console.log(nameCollection)
-    return fetch(
-      `https://filmoteka2-11906-default-rtdb.europe-west1.firebasedatabase.app//${uid}/${nameCollection}.json?auth=${accessToken}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(Collection),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    )
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }
-    // readFromFBHundler(dataUser.accessToken,'films',dataUser.uid)
-  function readFromFBHundler(accessToken,nameCollection,uid) {
-    return fetch(
-      `https://filmoteka2-11906-default-rtdb.europe-west1.firebasedatabase.app//${uid}/${nameCollection}.json?auth=${accessToken}`,
-    )
-      .then(response => response.json())
-      .then(response => console.log(response))
-  }
+console.log(dataUser.uid)
+if(dataUser !== null){
+    writeUserData(dataUser.uid,[34545667788,345678,34324234])
+    // writeToFBHundler(dataUser.accessToken,'films',dataUser.uid,[34545667788,345678,34324234])
+}
+// function writeToFBHundler(accessToken,nameCollection,uid,Collection) {
+//     console.log(nameCollection)
+//     return fetch(
+//       `https://filmoteka2-11906-default-rtdb.europe-west1.firebasedatabase.app//${uid}/${nameCollection}.json?auth=${accessToken}`,
+//       {
+//         method: 'POST',
+//         body: JSON.stringify(Collection),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       },
+//     )
+//       .then(response => response.json())
+//       .then(data => console.log(data));
+//   }
+  if(dataUser !== null){
+    // readUserData(dataUser.uid)
+    //    readFromFBHundler(dataUser.accessToken,'films',dataUser.uid)
+}
+
+//   function readFromFBHundler(accessToken,nameCollection,uid) {
+//     return fetch(
+//       `https://filmoteka2-11906-default-rtdb.europe-west1.firebasedatabase.app//${uid}/${nameCollection}.json?auth=${accessToken}`,
+//     )
+//       .then(response => response.json())
+//       .then(response => console.log(response))
+//   }
