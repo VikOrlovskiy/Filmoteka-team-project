e4fbef7501021abfbc2f5e71a9406b2ccae25478
vik_orel <vikorel34@gmail.com>
2022-01-20 13:31:39 +0200

    Update serchFilmByValue.js

--- src/js/serchFilmByValue.js
+++ src/js/serchFilmByValue.js
@@ -3,7 +3,6 @@ import { renderMovieCardFilms } from './normaliseRenderFilm';
 const dataFetch = new DataFetch();
 import ref from './Refs';
 import { showLoader, hideLoader } from './loader'
-import renderPopUpContent from './renderPopUpContent';
 ref.buttonFilmSerch.disabled = true;
 
 ref.formFilmSerch.addEventListener('submit', onSearch);
@@ -11,21 +10,11 @@ ref.inputFilmSerch.addEventListener('input', onSearchInput);
 function onSearch (e)  {
   e.preventDefault();
   dataFetch.query = e.currentTarget.elements.query.value.trim();
+  ref.galleryRef.innerHTML="" 
     dataFetch
       .fetchFilms()
-      .then(renderMovieCardFilms).then(renderPopUpContent)
+      .then(renderMovieCardFilms)
       .catch(error => {
         console.log(error.message);
       });
 };
-
-function onSearchInput(e) {
-  e.preventDefault();
-  if (!e.currentTarget.value) {
-    ref.buttonFilmSerch.disabled = true;
-    dataFetch.fetchTopFilms().then(renderMovieCardFilms)
-  }
-  else {
-    ref.buttonFilmSerch.disabled = false;
-  }
-}
