7b8f9393822e1959023b8860a48214c2a1096bd6
SerhBondarenko <serhii.bondarenk@gmail.com>
2022-01-24 13:13:38 +0200

    bug-fix
    добавил уведомление если в библиотеке  нет фильмов

--- src/js/firebaseDataWork.js
+++ src/js/firebaseDataWork.js
@@ -19,11 +19,21 @@ function onClickReadDataFirebase(e) {
       console.log(valueReadUserWatched)
       ref.galleryRef.innerHTML = '';
       valueReadUserWatched.then(value => {
-        localStorage.setItem(`${e.target.dataset.action}`, JSON.stringify(value))
-        let values = Object.values(value);
-        values.filter((course, index, array) => array.indexOf(course) === index)
-          .map(e => {test.fetchFilmByID(e).then(result => {renderMovieCard(result);});
-          });
+         if (value === null) {
+           ref.galleryRef.innerHTML = `<li class="text-error">
+          <p>You don't have any movies added yet.</p></li>`;
+         }
+else{
+  localStorage.setItem(`${e.target.dataset.action}`, JSON.stringify(value));
+  let values = Object.values(value);
+  values
+    .filter((course, index, array) => array.indexOf(course) === index)
+    .map(e => {
+      test.fetchFilmByID(e).then(result => {
+        renderMovieCard(result);
+      });
+    });
+}
       });
       ref.BtnWatched.disabled = true;
         ref.BtnQueue.disabled = false;
@@ -35,14 +45,20 @@ function onClickReadDataFirebase(e) {
     const valueReadUserData = onCkickReadUserData(
       dataUser.accessToken,e.target.dataset.action,dataUser.uid,);
     valueReadUserData.then(value => {
-      localStorage.setItem(`${e.target.dataset.action}`, JSON.stringify(value));
-      let values = Object.values(value);
-       values.filter((value, index, array) => array.indexOf(value) === index)
-       .map(e => {
-        test.fetchFilmByID(e).then(result => {
-           renderMovieCard(result);
-         });
-       });
+      if (value === null) {
+        ref.galleryRef.innerHTML = `<li class="text-error">
+          <p>You don't have any movies added yet.</p></li>`;
+      }
+      else {
+        localStorage.setItem(`${e.target.dataset.action}`, JSON.stringify(value));
+        let values = Object.values(value);
+        values.filter((value, index, array) => array.indexOf(value) === index)
+          .map(e => {
+            test.fetchFilmByID(e).then(result => {
+              renderMovieCard(result);
+            });
+          });
+      }
     })
     ref.BtnWatched.disabled = false;
     ref.BtnQueue.disabled = true;
