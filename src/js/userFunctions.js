e3526bb569270375a3528b9e402c5b25aa604f38
vik_orel <vikorel34@gmail.com>
2022-01-25 15:54:25 +0200

    Update userFunctions.js

--- src/js/userFunctions.js
+++ src/js/userFunctions.js
@@ -1,20 +1,22 @@
 import { authWithEmailAndPassword, RegistrationWithEmailAndPassword,logOutAuthUser,authState,readUserData,writeUserData} from "./fireBaseApi";
-import Refs from "./refs";
+import ref from "./refs";
 import {actionPopUp} from './actionPopUp'
 import formLogIn from '../templates/formLogIn.hbs';
 import formRegistration from '../templates/formRegistration.hbs';
 import Notiflix from 'notiflix';
+
+console.log(ref)
 // ==================logOut========================
-Refs.logOutButton.addEventListener('click' , logOutAuthUser)
+ref.logOutButton.addEventListener('click' , logOutAuthUser)
 // ==================LogIn========================
-Refs.logInButton.addEventListener('click' , onClickregistrationOrlogInUser)
+ref.logInButton.addEventListener('click' , onClickregistrationOrlogInUser)
 // ==================User auth State========================
 const emailCheck = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 
 authState()
 function onClickregistrationOrlogInUser(){
     renderForm(formLogIn)
-    Refs.popUp.classList.add("modal_form")
+    ref.popUp.classList.add("modal_form")
     document.body.classList.add('show-modal');
     let form =  document.querySelector('.form')
     form.addEventListener('submit',onSubmitEntryForm)
@@ -22,7 +24,7 @@ function onClickregistrationOrlogInUser(){
     actionPopUp()
 }
 function renderForm(value){
-    Refs.popUp.insertAdjacentHTML('afterbegin', value())
+    ref.popUp.insertAdjacentHTML('afterbegin', value())
 }
 function onSubmitEntryForm(e){
     e.preventDefault()
@@ -42,7 +44,7 @@ function onSubmitEntryForm(e){
     authWithEmailAndPassword(email, password);
     this.reset()
     document.body.classList.remove('show-modal');
-    Refs.popUp.innerHTML="" ;
+    ref.popUp.innerHTML="" ;
 }
 function onSubmitRegistrationForm(e){
     e.preventDefault()
@@ -73,10 +75,10 @@ function onSubmitRegistrationForm(e){
     RegistrationWithEmailAndPassword(email, password);
     this.reset()
     document.body.classList.remove('show-modal');
-    Refs.popUp.innerHTML="" ;
+    ref.popUp.innerHTML="" ;
 }
 function onRegistrationLinkClick(e){
-    Refs.popUp.innerHTML="" ;
+    ref.popUp.innerHTML="" ;
     renderForm(formRegistration)
     let form = document.querySelector('.form')
     form.addEventListener('submit', onSubmitRegistrationForm)
